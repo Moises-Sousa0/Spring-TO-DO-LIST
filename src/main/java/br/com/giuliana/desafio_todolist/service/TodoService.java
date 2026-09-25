c9e12c341b333262af7f0cd376c0bb3a433de1b7
Moises-Sousa0 <moisessousanow@gmail.com>
2026-09-24 21:40:35 -0300

    finalizando controller

--- src/main/java/br/com/giuliana/desafio_todolist/service/TodoService.java
+++ src/main/java/br/com/giuliana/desafio_todolist/service/TodoService.java
@@ -26,13 +26,13 @@ public class TodoService {
         return todoRepository.findAll(sort);
     }
 
-    public List<Todo> update(){
-
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
         return list();
     }
 
-    public List<Todo> delete(){
-
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
         return list();
     }
 }
